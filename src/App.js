687818dd9d1d975fa3a73895be47e9106906528a
shanmukha-thota <saishanmukha54@gmail.com>
2024-09-20 15:06:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am so hungry
+          I am so hungry....
         </p>
       </header>
     </div>
